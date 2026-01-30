15e5ff1da7a09a99751e8f2294f716fd80c43d1b
PC OSTRICH <a1197503@uabc.edu.mx>
2026-01-30 15:11:22 -0800

    Se realiza la configuracion de dependencias que requiere el proyecto

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,8 @@
 module com.example.Meta1ADS {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    requires org.mariadb.jdbc;
 
     opens Interfaz to javafx.fxml;
     exports Main;
